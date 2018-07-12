038ea92e47b4a47a83cc88f96e36c15a767f5c54
刘宇涵 <1106741606@qq.com>
2018-07-12 23:28:35 +0800

    调整缩进
    调整缩进

--- LiuYuHan/RSA/src/RSA.java
+++ LiuYuHan/RSA/src/RSA.java
@@ -8,39 +8,39 @@ public class RSA {
 		return RSAEncrypt.loadPublicKeyByFile(filepath);
 	}
 	//获取私钥的字符串形式
-		public static String getPriKey(String filepath) throws Exception {
-			return RSAEncrypt.loadPrivateKeyByFile(filepath);
-		}
+	public static String getPriKey(String filepath) throws Exception {
+		return RSAEncrypt.loadPrivateKeyByFile(filepath);
+	}
 	//公钥加密
 	public static String pubEncrypt(String publickey, String plainText) throws Exception {
-			byte[] cipherData=RSAEncrypt.encrypt(RSAEncrypt.loadPublicKeyByStr(publickey),plainText.getBytes());	
-			String cipher=Base64.encode(cipherData);	
-			return cipher;
+		byte[] cipherData=RSAEncrypt.encrypt(RSAEncrypt.loadPublicKeyByStr(publickey),plainText.getBytes());	
+		String cipher=Base64.encode(cipherData);	
+		return cipher;
 	}
 	//私钥解密
 	public static String priDecrypt(String privatekey, String cipher) throws Exception {
-			byte[] res=RSAEncrypt.decrypt(RSAEncrypt.loadPrivateKeyByStr(privatekey), Base64.decode(cipher));
-			String restr=new String(res);
-			return restr;
+		byte[] res=RSAEncrypt.decrypt(RSAEncrypt.loadPrivateKeyByStr(privatekey), Base64.decode(cipher));
+		String restr=new String(res);
+		return restr;
 	}
 	//私钥加密
 	public static String priEncrypt(String privatekey, String plainText) throws Exception {
-			byte[] cipherData=RSAEncrypt.encrypt(RSAEncrypt.loadPrivateKeyByStr(privatekey),plainText.getBytes());	
-			String cipher=Base64.encode(cipherData);	
-			return cipher;
+		byte[] cipherData=RSAEncrypt.encrypt(RSAEncrypt.loadPrivateKeyByStr(privatekey),plainText.getBytes());	
+		String cipher=Base64.encode(cipherData);	
+		return cipher;
 	}
 	//公钥解密
 	public static String pubDecrypt(String publickey, String cipher) throws Exception {
-			byte[] res=RSAEncrypt.decrypt(RSAEncrypt.loadPublicKeyByStr(publickey), Base64.decode(cipher));	
-			String restr=new String(res);	
-			return restr;
+		byte[] res=RSAEncrypt.decrypt(RSAEncrypt.loadPublicKeyByStr(publickey), Base64.decode(cipher));	
+		String restr=new String(res);	
+		return restr;
 	}
 	//签名
-		public static String sign(String content, String privateKey) {
-			return RSASignature.sign(content, privateKey);
-		}
+	public static String sign(String content, String privateKey) {
+		return RSASignature.sign(content, privateKey);
+	}
 	//验签
-		public static boolean doCheck(String content, String sign, String publicKey) {
-			return RSASignature.doCheck(content, sign, publicKey);
-		}
+	public static boolean doCheck(String content, String sign, String publicKey) {
+		return RSASignature.doCheck(content, sign, publicKey);
+	}
 }
